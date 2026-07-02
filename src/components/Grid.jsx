7b48964fe3a79bc8b1090e96717413dc72e7704d
The Hermit <goldendragonslayer20@gmail.com>
2026-07-02 17:04:56 +0000

    V2 card number

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 function Grid({ highlights, onOpen }) {
 
     return (
-        <div class="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 gap-8">
+        <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-5  gap-8">
             {
                 highlights.map((highlight) => (
                     <Card
